4efb8a4b1051f4d9d82fa2fd01abf6ef1dd6c0bb
urdhanaka <urdhanaka.aptanagi@gmail.com>
2025-04-13 20:11:18 +0700

    feat: add initialization process for node network interface

--- nodes-grpc/services/initialization.go
+++ nodes-grpc/services/initialization.go
@@ -1,32 +1,83 @@
 package services
 
 import (
-	"nodes-grpc/services/db"
+	"log/slog"
 	"nodes-grpc/services/virtualization"
+	"nodes-grpc/utils"
+
+	"github.com/vishvananda/netlink"
 )
 
 type Connection struct {
-	DockerService  *virtualization.DockerVirtualization
-	IncusService   *virtualization.IncusVirtualization
-	LibvirtService *virtualization.LibvirtVirtualization
+	VirtualizationService virtualization.VirtualizationInterface
 }
 
 func NewConnection() *Connection {
 	// connection
-	dbConnection := db.InitDB()
-	// dockerConnection := virtualization.InitDockerConnection()
 	incusConnection := virtualization.InitIncusConnection()
-	libvirtConnection := virtualization.InitLibvirtConnection()
 
 	// services
-	dbService := db.NewDBConnection(dbConnection)
-	// dockerService := virtualization.NewDockerVirtualization(dockerConnection, dbService)
-	incusService := virtualization.NewIncusVirtualization(incusConnection, dbService)
-	libvirtService := virtualization.NewLibvirtService(libvirtConnection, dbService)
+	incusService := virtualization.NewIncusVirtualization(incusConnection)
 
 	return &Connection{
-		// DockerService:  dockerService,
-		IncusService:   incusService,
-		LibvirtService: libvirtService,
+		VirtualizationService: incusService,
 	}
+}
+
+func CreateBridge() error {
+	// check if bridge already created
+	bridge, err := netlink.LinkByName(BRIDGE_NAME)
+	if err != nil {
+		return err
+	}
+
+	if bridge != nil {
+		return nil
+	}
+
+	// get used ethernet interface
+	ethInterface, err := utils.GetNodeUsedInterface()
+	if err != nil {
+		return err
+	}
+
+	eth, err := netlink.LinkByName(ethInterface)
+	if err != nil {
+		return err
+	}
+
+	bridgeLinkAttrs := netlink.NewLinkAttrs()
+	bridgeLinkAttrs.Name = BRIDGE_NAME
+
+	newBridge := &netlink.Bridge{LinkAttrs: bridgeLinkAttrs}
+	err = netlink.LinkAdd(newBridge)
+	if err != nil {
+		slog.Error("CreateBridge(): could not create network bridge",
+			"error", err.Error(),
+		)
+
+		return err
+	}
+
+	// add interfaces to bridge
+	err = netlink.LinkSetMaster(eth, newBridge)
+	if err != nil {
+		slog.Error("CreateBridge(): could not set network interfaces master, removing bridge...",
+			"error", err.Error(),
+		)
+
+		// remove the bridge
+		err := netlink.LinkDel(newBridge)
+		if err != nil {
+			slog.Error("CreateBridge(): could not remove the bridge",
+				"error", err.Error(),
+			)
+
+			return err
+		}
+
+		return err
+	}
+
+	return nil
 }
