8c23943df450d3095b58eece1c6c7445c245c90b
urdhanaka <urdhanaka.aptanagi@gmail.com>
2025-05-01 15:08:41 +0700

    refactor: files cleanup

--- nodes-grpc/common/model/virtualization/virtualization-model.go
+++
@@ -1,7 +0,0 @@
-package virtualization_model
-
-import "github.com/google/uuid"
-
-type InstanceIdentification struct {
-	InstanceID uuid.UUID
-}
--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-set -xeu
-
-# get helm script
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-
-# chmod
-chmod 700 get_helm.sh
