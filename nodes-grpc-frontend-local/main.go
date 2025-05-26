701dde096402b700f0dd53f45862f250cf0fea82
urdhanaka <urdhanaka.aptanagi@gmail.com>
2025-05-26 22:58:29 +0700

    refactor: remove unneeded code

--- nodes-grpc-frontend-local/main.go
+++ nodes-grpc-frontend-local/main.go
@@ -5,26 +5,5 @@ import (
 )
 
 func main() {
-	// nodeClient, err := config.NewNodeClient()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-	//
-	// slog.Info("spawning master...")
-	// _, err = nodeClient.CreateMaster(context.Background(), &proto_model.CreateMasterRequest{})
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-	//
-	// time.Sleep(time.Second * 20)
-	//
-	// slog.Info("spawning worker...")
-	// _, err = nodeClient.CreateWorker(context.Background(), &proto_model.CreateWorkerRequest{})
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
 	src.StartApp()
 }
