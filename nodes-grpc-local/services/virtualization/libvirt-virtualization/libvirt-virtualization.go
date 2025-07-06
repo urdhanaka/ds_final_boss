e58c95d5a489f7d57b66e33ccfcea6e46d8bdfae
urdhanaka <urdhanaka.aptanagi@gmail.com>
2025-07-06 23:33:42 +0700

    feat: updates

--- nodes-grpc-local/services/virtualization/libvirt-virtualization/libvirt-virtualization.go
+++ nodes-grpc-local/services/virtualization/libvirt-virtualization/libvirt-virtualization.go
@@ -187,7 +187,7 @@ func (c *LibvirtVirtualization) createMaster(
 	}
 
 	// getting the kubeconfig
-	getKubeConfigContentCmd := `cat /etc/rancher/k3s/k3s.yaml | head -c -1`
+	getKubeConfigContentCmd := fmt.Sprintf(`cat /etc/rancher/k3s/k3s.yaml | head -c -1 | sed 's/127.0.0.1/%s/g'`, string(decodedIpAddressBytes))
 	getKubeConfigContentStatus, err := guestAgentExecStatus(dom, getKubeConfigContentCmd)
 	if err != nil {
 		slogFunction(virtRequest.ClusterName, thisInstanceName, "error getting kubeconfig contents", err)
